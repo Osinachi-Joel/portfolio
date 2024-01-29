d3701e01c21aab1952557be8feec82f14c955738
ghostronin <osinachij161@gmail.com>
2024-01-29 18:19:26 +0100

    create and style the contact and footer sections

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,8 +1,25 @@
 import './footer.css'
+import {links, socials} from './data'
 
 const Footer = () => {
   return (
-    <footer id="footer"><h2>Footer</h2></footer>
+    <footer id="footer">
+      <div className="container footer_container">
+        <ul className='nav_menu'>
+          {
+            links.map(fLink => <li key={fLink.id}><a href={fLink.link}>{fLink.title}</a></li>)
+          }
+        </ul>
+        <div className="footer_socials">
+          {
+            socials.map(social => <a key={social.id} href={social.link} target="_blank" rel="noopener noreferrer">{social.icon}</a>)
+          }
+        </div>
+      </div>
+      <div className="footer_copyright">
+        <small>2024 Chinedu Osinachi Portfolio &copy; All Rights Reserved</small>
+      </div>
+    </footer>
   )
 }
 
